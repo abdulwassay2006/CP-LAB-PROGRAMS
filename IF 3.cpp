30277ed8f64d150f2ab192d493ce842ad357db3f
Abdul Wassay <abdulwasaay125@gmail.com>
2026-09-25 17:46:54 +0500

    Add files via upload

---
+++ IF 3.cpp
@@ -0,0 +1,16 @@
+// PROGRAM THAT confirms your richness 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	cout << " Enter your annual income in PKR ";
+	cin >> r;
+    	
+	if(r >= 3500000)
+	{
+		cout << " You are RICH ";
+	}
+	return 0;
+ } 

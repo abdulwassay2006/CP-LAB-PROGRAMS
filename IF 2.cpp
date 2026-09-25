c3df101c51209b5d3066175d8fc17b51b6ed4d64
Abdul Wassay <abdulwasaay125@gmail.com>
2026-09-25 17:39:42 +0500

    Add files via upload

---
+++ IF 2.cpp
@@ -0,0 +1,16 @@
+// PROGRAM THAT tells voting eligibility status
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int g;
+	cout << " Enter your age ";
+	cin >> g;
+    	
+	if(g >= 18)
+	{
+		cout << " You can vote ";
+	}
+	return 0;
+ } 

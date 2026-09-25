c12da1089842f0bb66a8fe861caf4c8663512724
Abdul Wassay <abdulwasaay125@gmail.com>
2026-09-25 17:55:40 +0500

    Add files via upload

---
+++ IF 4.cpp
@@ -0,0 +1,16 @@
+// PROGRAM THAT confirms even number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int g,e;
+	cout << " Enter a number ";
+	cin >> g;
+    	
+	if( g % 2 ==0)
+	{
+		cout << " Number is EVEN ";
+	}
+	return 0;
+ } 

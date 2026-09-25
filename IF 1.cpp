3c4cd60e010299d23059f2d7d92411fd962c8abc
Abdul Wassay <abdulwasaay125@gmail.com>
2026-09-25 16:27:28 +0500

    Add files via upload

---
+++ IF 1.cpp
@@ -0,0 +1,17 @@
+// Program that tells pass status if student passes based on marks
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m;
+	cout << "Enter Your marks = " ;
+	cin >> m;
+	
+	if(m>=50)
+	{
+		cout << "You are PASS" ;
+	}
+	
+	return 0;
+}
